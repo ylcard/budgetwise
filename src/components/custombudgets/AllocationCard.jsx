c6a8210220af9514fcfe3797b81f0db61c00cd3c
Vadim Riskin <vadimgarai@gmail.com>
2025-11-12 10:42:46 +0000

    Manual code change

--- src/components/custombudgets/AllocationCard.jsx
+++ src/components/custombudgets/AllocationCard.jsx
@@ -47,10 +47,10 @@ export default function AllocationCard({ allocation, category, stats, onEdit, on
               </div>
             </div>
             <div className="flex gap-1">
-              <Button variant="ghost" size="icon" onClick={onEdit} className="hover:bg-blue-50 hover:text-blue-600  h-8 w-8">
+              <Button variant="ghost" size="icon" onClick={onEdit} className="hover:bg-blue-50 hover:text-blue-600  h-7 w-7 rounded flex items-center justify-center">
                 <Pencil className="w-3 h-3" />
               </Button>
-              <Button variant="ghost" size="icon" onClick={onDelete} className="hover:bg-blue-50 hover:text-red-700 text-red-600 h-8 w-8">
+              <Button variant="ghost" size="icon" onClick={onDelete} className="hover:bg-red-50 hover:text-red-600 h-7 w-7 rounded flex items-center justify-center">
                 <Trash2 className="w-3 h-3" />
               </Button>
             </div>
