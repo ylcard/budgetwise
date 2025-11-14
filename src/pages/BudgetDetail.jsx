99f298a093ef87a4fa0eb2176790c2dd17fde193
Vadim Riskin <vadimgarai@gmail.com>
2025-11-14 09:48:46 +0000

    Manual code change

--- src/pages/BudgetDetail.jsx
+++ src/pages/BudgetDetail.jsx
@@ -655,7 +655,6 @@ export default function BudgetDetail() {
                             // UPDATED 16-Jan-2025: Replaced Button with CustomButton variant="delete"
                             <CustomButton
                                 variant="delete"
-                                type="button"
                                 onClick={handleDeleteBudget}
                             >
                                 <Trash2 className="w-4 h-4 mr-2" />
