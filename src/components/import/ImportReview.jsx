26402e570c30102f92cb4528ff7992a30b4dcea6
Vadim Riskin <vadimgarai@gmail.com>
2025-11-22 16:55:33 +0000

    Manual code change

--- src/components/import/ImportReview.jsx
+++ src/components/import/ImportReview.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
