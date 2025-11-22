93ac94778014c9eb17f390753d9a8fdf3af0d444
Vadim Riskin <vadimgarai@gmail.com>
2025-11-22 10:32:38 +0000

    Manual code change

--- src/components/categories/CategoryForm.jsx
+++ src/components/categories/CategoryForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { CustomButton } from "@/components/ui/CustomButton";
