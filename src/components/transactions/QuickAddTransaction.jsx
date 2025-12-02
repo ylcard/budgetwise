782267c9070706c8113d9e7152a659edd650a917
Vadim Riskin <vadimgarai@gmail.com>
2025-12-02 13:07:27 +0000

    Manual code change

--- src/components/transactions/QuickAddTransaction.jsx
+++ src/components/transactions/QuickAddTransaction.jsx
@@ -112,7 +112,6 @@ export default function QuickAddTransaction({
                     onSubmit={handleSubmit}
                     onCancel={handleCancel}
                     isSubmitting={isSubmitting}
-                    // cashWallet={cashWallet}
                     transactions={transactions}
                 />
             </DialogContent>
