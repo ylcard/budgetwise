601569bc0d428f9a7d68093e2281d597e50dcf14
Vadim <42947636+ylcard@users.noreply.github.com>
2026-02-13 14:28:12 +0100

    Delete src/components/utils/formatDate.jsx

--- src/components/utils/formatDate.jsx
+++
@@ -1,2 +0,0 @@
-// DEPRECATED 11-Nov-2025: This file's functionality has been moved to components/utils/dateUtils.js for better organization.
-// All imports should now use: import { formatDate } from '../utils/dateUtils';
