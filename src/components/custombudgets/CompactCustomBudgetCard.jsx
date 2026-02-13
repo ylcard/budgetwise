ec85b7211ac56d7d605b31bd939eb60ba2c1cf98
Vadim <42947636+ylcard@users.noreply.github.com>
2026-02-13 14:23:44 +0100

    Delete src/components/custombudgets/CompactCustomBudgetCard.jsx

--- src/components/custombudgets/CompactCustomBudgetCard.jsx
+++
@@ -1,5 +0,0 @@
-// DEPRECATED (2025-01-11): This file has been renamed to components/budgets/BudgetCard.jsx
-// Reason: The name "CompactCustomBudgetCard" was misleading as it handles both system and custom budgets
-// New location: components/budgets/BudgetCard.jsx
-// This file is scheduled for deletion after all import references are updated globally
-// DO NOT USE THIS FILE - Import from components/budgets/BudgetCard.jsx instead
